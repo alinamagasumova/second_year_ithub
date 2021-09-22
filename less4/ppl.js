1a5369d4f925e711f3d6408c11a4c97c4f64615c
Alina <malinka2002@mail.ru>
2021-09-22 15:07:05 +0300

    complete

--- less4/ppl.js
+++ less4/ppl.js
@@ -1,11 +1,13 @@
-let fs = require('fs');
-let Person = {
-    name: '',
-    surname: '',
-    docname: '',
-    birthDate: '',
-    gender: ''
+let Person = class {
+    constructor(name, surname, gender, birthDate) {
+        this.name = name;
+        this.surname = surname;
+        this.docName = `${name.charAt(0)}` + '. ' + `${surname}`;
+        this.gender = gender;
+        this.birthDate = birthDate;
+    }
 }
+let fs = require('fs');
 let text = fs.readFileSync('ppl.csv', 'utf8');
 let arr = text.split('\n');
 arr.shift();
@@ -16,5 +18,17 @@ arr.forEach((e) => {
 });
 arrObj.forEach((e) => {
     e.pop()
-})
-console.log(arrObj)
+});
+
+let people = [];
+for (let i=0; i<arrObj.length; i++) {
+    people.push(new Person(...arrObj[i]));
+};
+
+let male = [];
+for (let i = 0; i < people.length; i++) {
+    if (people[i].gender === "male") {
+        male.push(people[i].docName);
+    }
+ }
+
