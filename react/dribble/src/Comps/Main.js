aab0abc73f40a2ce7eb19d77ad368601d0f82f6f
Alina <malinka2002@mail.ru>
2022-03-09 09:57:05 +0200

    new dribble

--- react/dribble/src/Comps/Main.js
+++ react/dribble/src/Comps/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Cup from "../imgs/cup.png";
 import Bott from "../imgs/bott.png";
 import Kastr from "../imgs/kastr.png";
+import Circle from "../imgs/circle.png";
 
 function Main() {
     return (
@@ -14,6 +15,7 @@ function Main() {
 
             <div style={{"position": "relative"}}>
             <img src={Cup} alt='cup' className="imgCup"/>
+            <a href=''><img src={Circle} alt='circle' className="imgCircle"/></a>
             <a href="" className="allPr">VIEW ALL PRODUCTS</a>
             <div className='kastrDiv'><img src={Kastr} alt='kastr' className="imgKastr"/><p>Every item created in our studio is carefully crafted with a focus on the smallest detail. The art of pottery is our inspiration and passion.</p></div>
             </div>
